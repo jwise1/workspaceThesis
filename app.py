e5d3d8c93a3f29130c0d41d46ed25a32560f97aa
jwise1 <91438705+jwise1@users.noreply.github.com>
2024-11-18 15:27:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def main():
     st.set_page_config(page_title='Using Song Lyrics To Generate Memorable Passwords And Analysis Of Their Security', page_icon="")
 
     st.title("Using Song Lyrics To Generate Memorable Passwords And Analysis Of Their Security")
-    st.image("./VV3.jpg",caption="Verse Vault uses machine learning to generate passwords from song lyrics and rank the security of them based on cyberattack prediction")
+    st.image("./VV3.jpg",caption="Verse Vault uses machine learning to generate passwords from song lyrics and rank the security of them based on cyberattack prediction", width=10)
 
     # input as model generation, ability to edit from user
     if 'bPW' not in st.session_state or 'bPW1' not in st.session_state:
