9ebc9646afed2583aeabf531ae237ab8aa168154
jwise1 <91438705+jwise1@users.noreply.github.com>
2024-11-20 15:48:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def loadTokenizer():
 
 @st.cache_resource
 def loadModel():
-    return AutoModelForSeq2SeqLM.from_pretrained("jwise1/mt5-lyricPassword", token="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")
+    return AutoModelForSeq2SeqLM.from_pretrained("jwise1/mt5-lyricPassword", token=st.secrets['hf_mt5'])
     #genModel = AutoModelForSeq2SeqLM.from_pretrained("spiece.model")
 
 #@st.cache_resource
